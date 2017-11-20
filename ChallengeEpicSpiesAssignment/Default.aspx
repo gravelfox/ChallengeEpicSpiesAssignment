d50334164c8d336d9ebc8487bc932ef7e52b8e0d
Travis Brooks <tbrooks3141@gmail.com>
2017-11-20 13:29:52 -0800

    Initial Commit

---
+++ ChallengeEpicSpiesAssignment/Default.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" maintainScrollPositionOnPostBack="true"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 190px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <img class="auto-style1" src="epic-spies-logo.jpg" /><br />
+        <br />
+        <h1>Spy New Assignment Form</h1>
+        <p>
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="codeNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            New Assignment Name:&nbsp;
+            <asp:TextBox ID="assignmentTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            End Date of previous assignment:</p>
+        <p>
+            <asp:Calendar ID="prevEndCal" runat="server" ></asp:Calendar>
+        </p>
+        <p>
+            Start date of new assignment:</p>
+        <p>
+            <asp:Calendar ID="newStartCal" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            Projected end date of new assignment:</p>
+        <p>
+            <asp:Calendar ID="newEndCal" runat="server"></asp:Calendar>
+        </p>
+        <p>
+            <asp:Button ID="assignButton" runat="server" OnClick="assignButton_Click" Text="Assign Spy" />
+        </p>
+        <p>
+            <asp:Label ID="msgLabel" runat="server"></asp:Label>
+        </p>
+    
+    </div>
+    </form>
+    <p>
+        &nbsp;</p>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                prevEndCal.SelectedDate = DateTime.Now.Date;
+                newStartCal.SelectedDate = DateTime.Now.Date.AddDays(14);
+                newEndCal.SelectedDate = DateTime.Now.Date.AddDays(21);
+                newStartCal.VisibleDate = newStartCal.SelectedDate;
+                newEndCal.VisibleDate = newEndCal.SelectedDate;
+            }
+        }
+
+        protected void assignButton_Click(object sender, EventArgs e)
+        {
+            //ensure spy name and assignment name are entered and return error message if not
+            if (codeNameTextBox.Text == "" || assignmentTextBox.Text == "")
+            {
+                string errorMsg = "";
+                if (codeNameTextBox.Text == "")
+                {
+                    if (assignmentTextBox.Text == "")
+                    {
+                        msgLabel.Text = "You must specify a spy name and an assignment name.";
+                    }
+                    else
+                    {
+                        msgLabel.Text = "You must specify a spy name.";
+                    }
+  
+                }
+                else
+                {
+                    msgLabel.Text = "You must specify an assignment name.";
+                }
+
+            }
+            //ensure 14-day window between assignments, return error message if not, reset start date
+            else if (newStartCal.SelectedDate.Subtract(prevEndCal.SelectedDate).Days < 14)
+            {
+                msgLabel.Text = "Spy must have 14 days between end of previous assignment and start of new assignment.";
+                newStartCal.SelectedDate = DateTime.Now.Date.AddDays(14);
+                newStartCal.VisibleDate = newStartCal.SelectedDate;
+
+            }
+            else
+            //calculate budget and display to user
+            {
+                int budget = newEndCal.SelectedDate.Subtract(newStartCal.SelectedDate).Days * 500;
+                budget += (newEndCal.SelectedDate.Subtract(newStartCal.SelectedDate).Days > 21) ? 1000 : 0;
+                msgLabel.Text = string.Format("Spy Budget for <b>{0}</b> on <b>{1}</b>: {2:C}", codeNameTextBox.Text, assignmentTextBox.Text, budget);
+            }
+        }
+    }
+}
